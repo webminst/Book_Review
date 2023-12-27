4c2c08003549fbf390688d4f4b3d20a835288100
webminst <webminst@hotmail.com>
2023-12-27 11:47:27 -0300

    Projeto inicial

---
+++ pages/book_review.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(layout='wide')
+
+df_reviews = pd.read_csv('customer reviews.csv')
+df_top100_books = pd.read_csv('Top-100 Trending Books.csv')
+
+books = df_top100_books['book title'].unique()
+book = st.sidebar.selectbox('Books', books)
+
+df_book = df_top100_books[df_top100_books['book title'] == book]
+df_review_f = df_reviews[df_reviews['book name'] == book]
+
+book_title = df_book['book title'].iloc[0]
+book_genre = df_book['genre'].iloc[0]
+book_price = f"${df_book['book price'].iloc[0]}"
+book_rating = df_book['rating'].iloc[0]
+book_year = df_book['year of publication'].iloc[0]
+
+st.title(book_title)
+st.subheader(book_genre)
+col1, col2, col3 = st.columns(3)
+col1.metric('Price', book_price)
+col2.metric('Rating', book_rating)
+col3.metric('PriceYear of Publication', book_year)
+
+st.divider()
+
+for row in df_review_f.values:
+    st.write(f'**{row[2]}**')
+    st.write(row[5])
+
+# df_book
+# df_review_f
